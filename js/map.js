baaa89dbb98a70d48868926ef64f03ec3dd4e5b0
Sergei39Bugaev <arhi-kalip@mail.ru>
2022-08-22 12:58:55 +0200

    settings

---
+++ js/map.js
@@ -0,0 +1,32 @@
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [54.353647631616454, 18.658004604854707],
+            zoom: 16
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'Это красивая метка'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/location.svg',
+            // Размеры метки.
+            iconImageSize: [50, 50],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
+        });
+
+    myMap.geoObjects
+        .add(myPlacemark);
+});
